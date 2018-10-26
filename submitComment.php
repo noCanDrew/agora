4899ff3185e0a2887f35ddff1506acd2c25b00b0
Andrew Meyer <42983535+noCanDrew@users.noreply.github.com>
2018-10-25 18:03:25 -0700

    Update submitComment.php

--- submitComment.php
+++ submitComment.php
@@ -1,43 +1,43 @@
 <?php
-    /*
-        - Store comment made by user in "comment" table of agora DB
-    */
+	/*
+		- Store comment made by user in "comment" table of agora DB
+	*/
 	session_start();
 	include "library/dbInterface.php";
 
-    $header = 'Location: article.php';
+	$header = 'Location: article.php';
 	if(!empty($_POST["objectId"])) $objectId = strip_tags(trim($_POST['objectId']));
-    else $objectId = 0;
+	else $objectId = 0;
 
-    if(!empty($_POST["submit"]) && 
-    !empty($_POST["newComment"]) && 
-    !empty($_POST["parentId"]) &&
-    !empty($_POST["objectId"])){
-    	$comment = $_POST['newComment'];
-    	$comment = htmlspecialchars($comment, ENT_QUOTES);
-    	$comment = strip_tags(trim($comment));
+	if(!empty($_POST["submit"]) && 
+	!empty($_POST["newComment"]) && 
+	!empty($_POST["parentId"]) &&
+	!empty($_POST["objectId"])){
+		$comment = $_POST['newComment'];
+		$comment = htmlspecialchars($comment, ENT_QUOTES);
+		$comment = strip_tags(trim($comment));
 
-    	$parentId = strip_tags(trim($_POST['parentId']));
-    	if($parentId == "none") $parentId = 0;
-    	$objectId = strip_tags(trim($_POST['objectId']));
-    	$header .= '?article=' . $objectId;
+		$parentId = strip_tags(trim($_POST['parentId']));
+		if($parentId == "none") $parentId = 0;
+		$objectId = strip_tags(trim($_POST['objectId']));
+		$header .= '?article=' . $objectId;
 
-    	if($comment != ""){
-            $table = "comment";
-            $cols = array("objectId",  "parentId", "text");
-            $vals = array($objectId, $parentId, $comment);
-            if(dbInsert($table, $cols, $vals, $dbc)){
-                $header .= '&succss=1';
-            } else {
-                $header .= '&error=0';
-            }
-    	} else {
-    		$header .= '&error=1';
-    	}
-    } else {
-    	$header .= '?article=' . $objectId;
-        $header .= '&error=2';
-    }
+		if($comment != ""){
+			$table = "comment";
+			$cols = array("objectId",  "parentId", "text");
+			$vals = array($objectId, $parentId, $comment);
+			if(dbInsert($table, $cols, $vals, $dbc)){
+				$header .= '&succss=1';
+			} else {
+				$header .= '&error=0';
+			}
+		} else {
+			$header .= '&error=1';
+		}
+	} else {
+		$header .= '?article=' . $objectId;
+		$header .= '&error=2';
+	}
 	header($header);
 	exit();  
 ?>
